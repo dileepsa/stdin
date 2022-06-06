306c09afc610a4564b6e807579e2cf6f8952ca22
Dileep A <99489054+dileepsa@users.noreply.github.com>
2022-06-06 15:36:51 +0530

    writing to json file.

--- practice.js
+++ practice.js
@@ -1,12 +1,16 @@
+const fs = require('fs');
+
 process.stdin.setEncoding('utf-8');
 
 const readInput = (cb) => {
   let input = '';
   let index = 0;
+  const details = ['name', 'DOB', 'hobbies'];
   process.stdin.on('data', (chunk) => {
+    console.log('Please enter your', details[index]);
     input += chunk;
     index++;
-    if (index === 3) {
+    if (index === 4) {
       console.log(cb(input.split('\n')));
       process.exit(0);
     }
@@ -14,11 +18,12 @@ const readInput = (cb) => {
 };
 
 const toJson = (allInputs) => {
-  return {
+  const obj = {
     name: allInputs[0],
     DOB: allInputs[1],
     hobbies: allInputs[2].split(',')
   }
+  fs.writeFileSync('details.json', JSON.stringify(obj), 'utf8');
 }
 
 readInput(toJson);
